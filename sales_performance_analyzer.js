0d490ba681e9b258fd7af1b45d2a9d8ad1439732
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-09-08 17:13:40 -0400

    Develop findTopAndBottomPerformers function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -13,3 +13,23 @@ function determinePerformanceRating(averageSales) {
     else return "Needs Improvement";  // Sales amounts under 4,000
 }
 
+// Task 3: Create a Function to Identify Top and Bottom Performers
+function findTopAndBottomPerformers(salesreps) {
+    if (salesreps.length ===0) return {topPerformer: null, bottomPerformer: null };
+
+    // Utilize reduce to find top and bottom performers
+    return salesreps.reduce((result, individual) => {
+       
+       // Used to see if an individual is the top performer
+        if (result.topPerformer === null || individual.totalSales > result.topPerformer.totalSales) {
+            result.topPerformer = individual;
+        } 
+
+        // Used to see if an individual is the bottom performer
+        if (result.bottomPerformer === null || individual.totalSales < result.bottomPerformer.totalSales) {
+            result.bottomPerformer = individual;
+        }
+return result;
+    },{ topPerformer: null, bottomPerformer: null });
+}
+
